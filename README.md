53851100858ae7af52a208c45697b459e6594fea
Cole Striler <colestriler@gmail.com>
2021-11-12 13:28:22 -0800

    Setup Postgres schema

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# 5str-rvw
+
+
+## Database
+I designed a relational schema (Postgres) and created a one-to-many relationship between the Product and Review tables. One product can have many reviews.
+
+SQLAlchemy is used as the ORM.
+
+I used the same schema for both the MVP and V2.
+
+My initial idea was to create two separate Review tables (i.e.`ReviewMVP` and `ReviewV2`) so that the MVP reviews would be restricted to integers, and the V2 reviews would be floats.
+
+But to limit the overhead, I decided to handle this logic in the XXX route and use the same db for both the MVP and V2. 
+
+I figured a float could be used to represent whole numbers, but an integer couldn't be used to represent decimals.
--- app.py
+++ app.py
@@ -1,11 +1,41 @@
+import os
+from datetime import datetime
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dataclasses import dataclass
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('GUMROAD_SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('GUMROAD_DATABASE_URL')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@dataclass
+class Product(db.Model):
+    id: int = db.Column(db.Integer, primary_key=True)
+    name: str = db.Column(db.String(100), nullable=False)
+    reviews: list = db.relationship('Review', backref='product', lazy=True)
+
+    def __repr__(self):
+        return f"Product('{self.name}')"
+
+@dataclass
+class Review(db.Model):
+    id: int = db.Column(db.Integer, primary_key=True)
+    date: datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    product_id: str = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    description: str = db.Column(db.Text, nullable=True)
+    rating: float = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"Review('{self.rating}', '{self.description}')"
 
 
 if __name__ == '__main__':
